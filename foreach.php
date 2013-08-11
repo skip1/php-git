bce76e45baa4d00963139c4e096857d894d45678
skip1 <smkipper@gmail.com>
2013-08-10 23:46:15 -0400

    foreach first commit

---
+++ foreach.php
@@ -0,0 +1,8 @@
+<?php
+ $employeeAges;
+ $employeeAges['shmeel'] = 31;
+ $employeeAges['yankel'] = 22;
+ $employeeAges['gimpel']= 29;
+ foreach ($employeeAges as $key =>$value)  {
+  echo "my Name is  ($key) and my age is ($value) <br/>";}
+?>
