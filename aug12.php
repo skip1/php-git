14fdea51fd904b52d1a1860933372fde469d3543
skip1 <smkipper@gmail.com>
2013-08-12 15:03:39 -0400

    aug12 3rd draft

--- aug12.php
+++ aug12.php
@@ -1,9 +1,9 @@
 <?php
- session_start();
- if (isset($_SESSION[]) && $_SESSION['REQUEST_METHOD'] == 'POST') {
- $_SESSION['count'] = 0;
- } else {
- $_SESSION['count'] ++;
+ // session_start();
+ //if (isset($_SESSION[]) && $_SESSION['REQUEST_METHOD'] == 'POST') {
+ //$_SESSION['count'] = 0;
+ //} else {
+ //$_SESSION['count'] ++;
  
   $transaction = array();
   $transaction['type'] = $_POST['type'];
@@ -23,37 +23,43 @@
   class account  {
   public $starting_balance;
   public $current_balance;
-  public $transactions = array();
-  }
+  private $transactions = array();
+  
   
 
  
 
 
-  function __construct($amount)  {
+  public function __construct($amount)  {
  $this->starting_balance = $amount;
  $this->current_balance = $amount; 
  }
-  function debit ($amount)  {
+   public function debit ($amount)  {
   $transaction = array();
   $transaction[amount] = ($amount);
   $transaction[type] = 'debit';
   
   $this->transactions[]=$transaction;
+}
 
-
-  function credit ($amount) {
+   public function credit ($amount) {
   $transaction['amount'] = $amount; 
   $transaction['type'] = ('credit');
- $this->transactions[] = $transaction;
+  $this->transactions[] = $transaction;
    }
+
+  public function process()  {
+  echo 'type |  amount <br>';
+  foreach ($this->transactions as $transaction)  {
+  echo ($transaction['type']. ' | ' .  $transaction ['amount'] . '<br>';
  if($transaction['type'] == 'debit')  {
  $this->current_balance=$this->current_balance - $transaction['amount'];}
- else
+ else {
  $this->current_balance=$this->current_balance + $transaction['amount'];
-  } 
+  }}} 
  echo $current_balance;
- 
+ print_r $transaction
+ print_r $current_balance
 
 
  'Request method: ' . $_SERVER["REQUEST_METHOD'];
@@ -62,7 +68,7 @@
  <fieldset>
  <LABEL for = "amount">Amount: </LABEL>
  <INPUT type = "text" name = "amount" <BR>
- <IINPUT type = "radio" name = "type" value = "debit"> Debit<BR>
+ <INPUT type = "radio" name = "type" value = "debit"> Debit<BR>
  <INPUT type = "radio" name = "type" value = "credit"> Credit<BR>
  <INPUT type = "submit" value = "Send"> <INPUT type = "reset">
  </fieldset>
